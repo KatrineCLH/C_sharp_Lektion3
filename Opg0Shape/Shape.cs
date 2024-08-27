15360fda5276daf663a49fc88aa0a34961cb9a93
katrine <eaa23kch@students.eaaa.dk>
2024-08-27 20:18:30 +0200

    Add project files.

---
+++ Opg0Shape/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg0Shape
+{
+    internal class Shape
+    {
+        private double x;
+        private double y;
+
+        public double X 
+        { 
+            get { return x; } 
+            set { x = value; } 
+        }
+        public double Y 
+        { 
+            get { return y; } 
+            set { y = value; } 
+        }
+
+        public Shape(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Shape()
+        {
+            
+        }
+    }
+}
